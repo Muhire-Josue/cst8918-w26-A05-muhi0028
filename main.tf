c490ca97a1d44a9951609059e293aef3af246c2f
Muhire-Rutayisire <muhi0028@algonquinlive.com>
2026-02-27 19:02:45 -0500

    Add virtual network

--- main.tf
+++ main.tf
@@ -29,3 +29,19 @@ resource "azurerm_resource_group" "rg" {
   name     = "${var.labelPrefix}-A05-RG"
   location = var.region
 }
+
+resource "azurerm_public_ip" "pip" {
+  name                = "${var.labelPrefix}-A05-PIP"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  allocation_method = "Static"
+  sku               = "Standard"
+}
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.labelPrefix}-A05-VNET"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  address_space = ["10.0.0.0/16"]
+}
