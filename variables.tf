6731237cd186bc18fb8d7527df083f5963d5815e
Muhire-Rutayisire <muhi0028@algonquinlive.com>
2026-02-27 20:38:59 -0500

    Change default region to southafricanorth

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "labelPrefix" {
 variable "region" {
   description = "Azure region to deploy resources into."
   type        = string
-  default     = "canadacentral"
+  default     = "southafricanorth"
 }
 
 variable "admin_username" {
